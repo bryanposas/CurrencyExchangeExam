6a8b577d34a8d01093008cd8b3ca4daeae99b5fc
Bryan Posas <bryanposas@gmail.com>
2026-06-10 12:05:00 +0800

    Initial commit for UIKit lifecycle project.

---
+++ CurrencyExchangeExam/CurrencyExchangeExamAppDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  CurrencyExchangeExamApp.swift
+//  CurrencyExchangeExam
+//
+//  Created by Macintosh HD on 6/10/26.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // iOS 12 and earlier window (kept for compatibility)
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Nothing to do here; SceneDelegate will configure the window for iOS 13+
+        return true
+    }
+
+    // MARK: - UISceneSession Lifecycle
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let config = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        config.delegateClass = SceneDelegate.self
+        return config
+    }
+}
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        let rootVC = CurrencyExchangeViewController()
+        let nav = UINavigationController(rootViewController: rootVC)
+        window.rootViewController = nav
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+    func sceneWillResignActive(_ scene: UIScene) {}
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
+
---
+++ CurrencyExchangeExam/CurrencyExchangeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CurrencyExchangeViewController.swift
+//  CurrencyExchangeExam
+//
+//  Created by Macintosh HD on 6/10/26.
+//
+
+import UIKit
+
+class CurrencyExchangeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+}
